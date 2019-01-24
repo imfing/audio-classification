3096023387220a76fb7dd3961877f08495be1cac
mtobeiyf <mtobeiyf@163.com>
2019-01-25 00:46:45 +0800

    Fix some issues

--- cnn.py
+++ cnn.py
@@ -5,7 +5,7 @@
 # Date  : 2017-12-03
 #
 
-from feat_extract import *
+import feat_extract
 import time
 import argparse
 import numpy as np
@@ -67,6 +67,7 @@ def predict(args):
         predict_feat_path = 'predict_feat.npy'
         predict_filenames = 'predict_filenames.npy'
         filenames = np.load(predict_filenames)
+        X_predict = np.load(predict_feat_path)
         X_predict = np.expand_dims(X_predict, axis=2)
         pred = model.predict_classes(X_predict)
         for pair in list(zip(filenames, pred)): print(pair)
@@ -111,11 +112,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-t', '--train',             action='store_true',                           help='train neural network with extracted features')
     parser.add_argument('-m', '--model',             metavar='path',     default='trained_model.h5',help='use this model path on train and predict operations')
-    parser.add_argument('-e', '--epochs',            metavar='N',        default=1000,              help='epochs to train', type=int)
+    parser.add_argument('-e', '--epochs',            metavar='N',        default=500,              help='epochs to train', type=int)
     parser.add_argument('-p', '--predict',           action='store_true',                           help='predict files in ./predict folder')
     parser.add_argument('-P', '--real-time-predict', action='store_true',                           help='predict sound in real time')
     parser.add_argument('-v', '--verbose',           action='store_true',                           help='verbose print')
     parser.add_argument('-s', '--log-speed',         action='store_true',                           help='performance profiling')
-    parser.add_argument('-b', '--batch-size',        metavar='size',     default=64                 help='batch size', type=int)
+    parser.add_argument('-b', '--batch-size',        metavar='size',     default=64,                help='batch size', type=int)
     args = parser.parse_args()
     main(args)
